a5fc99549d1579d6107840380331cd529ae296a5
Arthur_Sauvage <77831535+Arthur-sauvage@users.noreply.github.com>
2022-05-04 15:36:50 +0200

    add connection with backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(express.json());
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("API is working !!");
+});
+
+var nb_users = 0;
+var nb_cumule = 0;
+
+router.post('/', function(req, res, next) {
+  const { card } = req.body;
+  nb_users ++;
+  nb_cumule += parseInt(card);
+  if (nb_users >= 5){
+    average = nb_cumule/5;
+    res.send("Average : " + average);
+    nb_users = 0
+  } 
+  
+  res.send("Confirmed Card : "+card);
 });
 
 module.exports = router;
